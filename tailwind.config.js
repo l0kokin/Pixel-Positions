a6bb7e2e80f4389391bcbce22829a0f76e5289c3
l0kokin <sali.gogishvili1@gmail.com>
2024-11-18 11:35:13 +0400

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,17 +3,20 @@ import defaultTheme from 'tailwindcss/defaultTheme';
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/**/*.blade.php',
-        './resources/**/*.js',
-        './resources/**/*.vue',
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
     ],
     theme: {
         extend: {
-            fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+            colors: {
+                'black': '#060606'
             },
+            fontFamily: {
+                'hanken-grotesk': ['Hanken Grotesk', 'sans-serif']
+            },
+            fontSize: {
+                "2xs": "0.625rem" //10px
+            }
         },
     },
     plugins: [],
